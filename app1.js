9b386a0f1861ac23884ff2218930a58b873497c5
Mehreenn <mehro.ar.002@gmail.com>
2021-10-19 20:32:39 -0400

    f commit

---
+++ app1.js
@@ -0,0 +1,72 @@
+
+document.getElementById('UI-form').addEventListener('submit', function (e) 
+{
+    //Hide results 
+    document.getElementById('results').style.display = 'none';
+    
+    //Time out
+    setTimeout(result, 1500);
+
+
+    e.preventDefault();
+});
+
+
+function result()
+{ 
+    const age = document.getElementById('age').value;
+    const weight = document.getElementById('weight').value;
+	
+    const height = document.getElementById('height').value;
+	
+    const gender = document.getElementById('gender').value;
+    const activity = document.getElementById('activity').value;
+    const outputBMR = document.getElementById('BMR');
+    
+
+    //Show results 
+    document.getElementById('results').style.display = 'block';
+    //Calculate BMR
+    
+    if (gender === 'male') 
+	{
+        let BMR = 66 + (13.75* weight) + ( 5.003 * height) - (6.755 * age)
+        outputBMR.value = BMR.toFixed(2);;
+        calculateTDEE(activity, BMR)
+    } 
+	else if (gender === 'female') 
+	{
+        let BMR = 655.1 + (9.563 * weight) + (1.85 * height) - ( 4.676 * age)
+        outputBMR.value = BMR.toFixed(2);;
+        calculateTDEE(activity, BMR)
+    }
+	
+	
+}	
+
+function calculateTDEE(activity, BMR)
+{
+    const outputTDEE = document.getElementById('TDEE');
+
+    if(activity == 'Sedentary'){
+        let TDEE = BMR * 1.2
+        outputTDEE.value = TDEE.toFixed(2);
+        }
+    else if (activity == 'Lightly Active') {
+        let TDEE = BMR * 1.375
+        outputTDEE.value = TDEE.toFixed(2);
+        }
+    else if (activity == 'Moderately Active'){
+        let TDEE = BMR * 1.55
+        outputTDEE.value = TDEE.toFixed(2);            
+        }
+    else if (activity == 'Very Active') {
+        let TDEE = BMR * 1.725
+        outputTDEE.value = TDEE.toFixed(2);
+    }
+    else{
+        let TDEE = BMR * 1.9
+        outputTDEE.value = TDEE.toFixed(2);
+    }
+
+}
